10590a65122cab7a85f1d846e2e29055fb42ffd0
chadra-adhikari <37656916+chadra-adhikari@users.noreply.github.com>
2018-03-25 19:14:29 +0530

    changes in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,13 +90,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-  :user_name => 'javed.hussain@mobiloitte.com',
-  :password => 'Mobiloitte1',
-  :domain => 'sendgrid.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true }
-
 end
